863117bdbf3d0320f3c8d3959ef70c747db5f7f2
MatyRN <reimondezmaty@gmail.com>
2024-02-07 15:23:53 -0300

    Termine Dados Player 1

--- src/commands/dados.ts
+++ src/commands/dados.ts
@@ -2,23 +2,47 @@ import type { MySlashCommand } from '../types';
 import { SlashCommandBuilder } from 'discord.js';
 import { setTimeout } from 'node:timers/promises';
 import utils from '../lib/Utils';
-
 export default {
   data: new SlashCommandBuilder()
-    .setName('dados')
-    .setDescription('TIRAR LOS DADOS'),
+    .setName('Dados')
+    .setDescription('Tira los dados, el que tire el numero mas alto gana'),
 
-  async run(interaction) {
-    const dado1 = Math.floor(Math.random() * 6) + 1;
-    const sleep = setTimeout;
-    // const dado2 = Math.floor(Math.random() * 6) + 1;
+    async run(interaction) {
+      const FrasesDeVictoria=["Sos un capo sabelo","Se te cayo la corona rey","Quiero se como vos cuando sea humano","Sos un grande pa","VIVA CRISTO REY"];
+      const FrasesDeDerrota=["Sos un pete","*Se ahoga con su baba y muere*","*Un pajaro te picotea el pito*","Taradini","Se me acabaron los bardeos","*Se cae a un poso por boludo*"]
+  
+      const Dado1 = Math.floor(Math.random() * 6) + 1;//El Dado de la PC
+      const sleep = setTimeout; //DELAY de CONTESTACION
+      const Dado2 = Math.floor(Math.random() * 6) + 1;//El Dado de la PC
 
-    await interaction.reply(`Tirando los dados...`);
+      await interaction.reply(`Tirando los dados.`);
+        await sleep(500);
+      await interaction.editReply(`Tirando los dados..`);
+        await sleep(500);
+      await interaction.editReply(`Tirando los dados...`);
 
-    await sleep(3000);
+      await interaction.editReply(`El dado de | ${interaction.member?.user?.username} | : ${Dado1}`);
+      await interaction.editReply(`Mi Turno Bro`);
 
-    await interaction.editReply(
-      `El dado de ${interaction.member?.user?.username}: ${dado1}`
-    );
-  },
+      await interaction.editReply(`Tirando los dados.`);
+        await sleep(500);
+      await interaction.editReply(`Tirando los dados..`);
+        await sleep(500);
+      await interaction.editReply(`Tirando los dados...`);
+
+      await interaction.editReply(`El dado de | CHESTER EL BOT | : ${Dado2}`);
+ 
+      await interaction.editReply(`Resultados Finales`);
+      //RESULTADOS FINALES
+      await sleep(500);
+        if(Dado1>Dado2){
+          await interaction.editReply("Ganaste" + interaction.member?.user?.username + " " + FrasesDeVictoria);
+          await interaction.editReply("Perdiste" + "CHESTER EL BOT" + " " + FrasesDeDerrota);
+        }else if(Dado1<Dado2){
+          await interaction.editReply("Perdiste" + interaction.member?.user?.username + " " + FrasesDeDerrota);
+          await interaction.editReply("Ganaste" + "CHESTER EL BOT" + " " + FrasesDeVictoria);
+        }else{
+          await interaction.editReply("Nose como pero ambos perdieron, y ganaron a la vez, deberian revisar este error los programadores :/");
+      }
+    },
 } satisfies MySlashCommand;
