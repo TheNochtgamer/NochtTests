6f8b3e84aa5b101b1bb1f7bebe33ab4fecb8025e
SomeUserK <141675084+SomeUserK@users.noreply.github.com>
2024-09-17 20:27:15 -0300

    Added rewards and perms

--- src/lib/Enums.ts
+++ src/lib/Enums.ts
@@ -89,15 +89,20 @@ export const AgsResponseTypes = Object.freeze({
 
 export const AgsPrizes = Object.freeze({
   'x1 Reward': '2be52b9b6d6ecc22e495866aa284c7a7.png',
-  'x6 Rewards': '69680b87c442703684e1a4b01aa61d1f.png',
   'x3 Rewards': '3922c0def0f6bb5d2df337e1024ef496.png',
+  'x6 Rewards': '69680b87c442703684e1a4b01aa61d1f.png',
+  'x9 Rewards': 'daea8e7f3da1eae85a723b5411035ef7.png',
   'Pin PS4': '4ed29b5e14f85131bad7caae470c2c10.png',
   'Remera PS4': '67ed9a9c193f57187f1ba9e24f3fef73.png',
   'Abre latas PS4': 'c31670020982bf968a6bcd8c88548165.png',
   'Latita Speed': 'dbf5604389e5636ad2001849b1648e94.png',
+  'Latita Speed Zero': '01f67c7978d815428808768245590c0b.png',
+  'Coca Cola': '230adef0a2a41e55df3b117df661b972.png',
+  'Coca Cola Zero': '8b1a4e16d5e12ec5ff8a10d2249384d6.png',
   "Llavero Assasin's Creed": 'e7c22c0b3f1bb068b2559a2bb93ea6cd.png',
   'Entrada Domingo': '879d5be8a6ad0495b02d6ba95b37cf41.png',
   'Entrada Sabado': '05b9fb0efdee532198a7daacaeb9a497.png',
   'Entrada Viernes': '510b621cfdd59eccc50ee6d6231221bc.png',
-  'Gamer Pack': '21f0f0511d22fe57b86f6f504fabc985.png'
-});
+  'Gamer Pack': '21f0f0511d22fe57b86f6f504fabc985.png',
+  'Mouse Pad AGS': '015489f53e307ef43ed15719c9fc48f5.png'
+} satisfies { [key: string]: string });
